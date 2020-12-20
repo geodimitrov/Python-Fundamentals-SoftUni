11c8f37927ad0b8ab7f02c754409264d5e066bcb
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-20 21:54:16 +0200

    Add files via upload

---
+++ Python Fundamentals/Basic Syntax, Conditional Statements & Loops/05. count_sheep.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Introduce input variable (should be an integer)
+
+number = int(input())
+
+#Create loop
+
+for i in range (1, number + 1):
+    print (f"{i} sheep...", end = "")
+
+
+# In[ ]:
+
+
+
+
