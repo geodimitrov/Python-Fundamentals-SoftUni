2e69a1c653446bd8d6e435ab887d5f9c75141722
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-20 20:01:29 +0200

    Add files via upload

---
+++ Python Fundamentals/Basic Syntax, Conditional Statements & Loops/02. have_a_drink.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+# 1. Create input (they don't specify whether "int" or "float" so we'll stick to "int")
+
+age = int(input())
+
+
+# 2. Create conditions
+
+if age <= 14:
+    drink_type = "toddy"
+elif 14 < age <= 18:
+    drink_type = "coke"
+elif 18 < age <= 21:
+    drink_type = "beer"
+else:
+    drink_type = "whisky"
+
+print(f"drink {drink_type}")
+
+
+# In[ ]:
+
+
+
+
