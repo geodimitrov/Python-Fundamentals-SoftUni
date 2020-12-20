ddcce280852ec0b740936d60f3e1cbcb867cabd2
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-20 20:09:29 +0200

    Add files via upload

---
+++ Python Fundamentals/Basic Syntax, Conditional Statements & Loops/03. leo_di_caprio's_oscars.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+# 1. Introduce input variable
+oscar_num = int(input())
+
+# 2. Introduce conditions
+
+if oscar_num == 88:
+    print ("Leo finally won the Oscar! Leo is happy")
+    
+elif oscar_num == 86:
+    print ("Not even for Wolf of Wall Street?!")
+    
+elif oscar_num < 88:
+    print ("When will you give Leo an Oscar?")
+    
+elif oscar_num > 88:
+    print ("Leo got one already!")
+
+
+# In[ ]:
+
+
+
+
