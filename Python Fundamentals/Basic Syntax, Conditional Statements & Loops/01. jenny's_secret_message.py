32bb4085df0af0467f2b1090f219ccd093f82def
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-22 20:39:31 +0200

    Update 01. jenny's_secret_message.py

--- Python Fundamentals/Basic Syntax, Conditional Statements & Loops/01. jenny's_secret_message.py
+++ Python Fundamentals/Basic Syntax, Conditional Statements & Loops/01. jenny's_secret_message.py
@@ -1,26 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
-
-#Let's warm up with something easy. 
-#Here, we want to write a program that greets all users BY NAME except for Johnny who will be greeted differently
 
 # 1.Introduce input variable
-
 user = input()
 
-# 2. Create conditions
-
+# 2.Create conditions
 if user == "Johnny":
     print("Hello, my love!")
 else:
     print(f"Hello, {user}!")
-
-
-# In[ ]:
-
 
 
 
