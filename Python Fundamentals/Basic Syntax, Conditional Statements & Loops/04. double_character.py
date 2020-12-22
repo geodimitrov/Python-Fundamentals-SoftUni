41c609fec3a5a5fb6e79bdce9c664a51330695f5
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-22 20:35:23 +0200

    Update 04. double_character.py

--- Python Fundamentals/Basic Syntax, Conditional Statements & Loops/04. double_character.py
+++ Python Fundamentals/Basic Syntax, Conditional Statements & Loops/04. double_character.py
@@ -1,20 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 # 1. Introduce input variable (string)
-
 string = input()
 
-# 2. Creat a for loop in order to double every character in the string
-
+# 2. Create a for loop in order to double every character in the string
 for char in range (0, len(string)):
     print (2*string[char], end ="")
 
-
-# In[ ]:
 
 
 
