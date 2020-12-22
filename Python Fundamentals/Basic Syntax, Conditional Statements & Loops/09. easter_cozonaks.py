542e65db4fadb54f7855eacde741c5fe9df28720
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-22 20:10:29 +0200

    Add files via upload

---
+++ Python Fundamentals/Basic Syntax, Conditional Statements & Loops/09. easter_cozonaks.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+# 1. Introduce variables
+
+budget = float(input())
+price_flour = float(input())
+colored_eggs = 0
+cozonaks = 0
+
+# 2. Calculate price of eggs, milk & cozonak
+
+price_eggs = price_flour * 0.75
+price_milk_lt = price_flour * 1.25 #we will use 0.250 l per cozonak
+price_cozonak = price_flour + price_eggs + price_milk_lt * 0.25
+
+# 3. Use while loop to calculate N of cozonaks you made + colored eggs and how much you've got left
+
+while budget >= price_cozonak:
+    cozonaks += 1
+    colored_eggs += 3
+    if cozonaks % 3 == 0:
+        colored_eggs -= cozonaks - 2
+    budget -= price_cozonak
+    
+# 4. Print the results
+
+print (f"You made {cozonaks} cozonacs! Now you have {colored_eggs} eggs and {budget:.2f}BGN left.")
+
+
+# In[ ]:
+
+
+
+
