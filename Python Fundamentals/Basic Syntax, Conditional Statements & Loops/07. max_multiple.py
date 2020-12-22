7ef6894b9cce032dff68f3e80c797af1683137c3
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-22 20:22:31 +0200

    Update 07. max_multiple.py

--- Python Fundamentals/Basic Syntax, Conditional Statements & Loops/07. max_multiple.py
+++ Python Fundamentals/Basic Syntax, Conditional Statements & Loops/07. max_multiple.py
@@ -1,26 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
 
 # 1. Introduce variables
-
 divisor = int(input())
 bound = int(input())
 N = bound
 
 # 2. Run a while loop that checks which is the largest int number (N) divisible by the divisor. Start from bound
-
 while bound % divisor != 0:
     bound -= 1
     N = bound
 
+# 3. Print result
 print (N)
-
-
-# In[ ]:
-
-
-
-
