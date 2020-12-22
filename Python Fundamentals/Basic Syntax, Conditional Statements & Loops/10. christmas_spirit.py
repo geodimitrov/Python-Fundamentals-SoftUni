dc2e7b151235183061bece16c47131ec6797d271
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-22 20:20:44 +0200

    Update 10. christmas_spirit.py

--- Python Fundamentals/Basic Syntax, Conditional Statements & Loops/10. christmas_spirit.py
+++ Python Fundamentals/Basic Syntax, Conditional Statements & Loops/10. christmas_spirit.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
 
 # 1. Introduce variables
 quantity = int(input())
@@ -18,14 +13,17 @@ for day in range (1, days + 1):
     if day % 2 == 0:
         total_cost += quantity * 2  
         christmas_spirit += 5
+        
     if day % 3 == 0:
         total_cost += quantity * 5 + quantity * 3   
         christmas_spirit += 13
         if day % 5 == 0:
             christmas_spirit += 30
+            
     if day % 5 == 0:
         total_cost += quantity * 15   
         christmas_spirit += 17
+        
     if day % 10 == 0:
         total_cost += 5 + 3 + 15
         christmas_spirit -= 20
@@ -35,10 +33,3 @@ for day in range (1, days + 1):
 # 3. Print the results      
 print (f"Total cost: {total_cost}")
 print (f"Total spirit: {christmas_spirit}")
-
-
-# In[ ]:
-
-
-
-
