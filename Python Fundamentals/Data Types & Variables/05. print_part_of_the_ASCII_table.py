a4212da1eea175c772b9d883f32aec160d337fc7
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-24 22:28:51 +0200

    Update 05. print_part_of_the_ASCII_table.py

--- Python Fundamentals/Data Types & Variables/05. print_part_of_the_ASCII_table.py
+++ Python Fundamentals/Data Types & Variables/05. print_part_of_the_ASCII_table.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
 
 start_num = int(input())
 end_num = int(input())
@@ -10,10 +5,4 @@ end_num = int(input())
 # use for loop to set the range and chr function to get the character
 for num in range (start_num, end_num + 1):
     print (f"{chr(num)} ", end= "")
-
-
-# In[ ]:
-
-
-
 
