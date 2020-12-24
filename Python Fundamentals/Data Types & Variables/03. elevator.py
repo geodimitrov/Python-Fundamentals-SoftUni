6f8981785cee8f2963cf0359e4666d5328704e40
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-24 22:08:46 +0200

    Update 03. elevator.py

--- Python Fundamentals/Data Types & Variables/03. elevator.py
+++ Python Fundamentals/Data Types & Variables/03. elevator.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
 
 # Solution 1
 # 1. Introduce variables
@@ -12,14 +7,12 @@ p = int(input())  #capacity of the elevator
 
 # 2. Use conditions to calculate result
 
-if n % p == 0:     #if there is no remainder when we / the n of ppl to the capacity
-    print(int(n / p))  # the required courses will be equal to the n of ppl / capacity
-else:              # if the first condition is not true
-     print ((n // p) + 1) #use floor division and add 1 more
+if n % p == 0:            # if there is no remainder when we / the n of ppl to the capacity
+    print(int(n / p))     # the required courses will be equal to the n of ppl / capacity
+else:                     # if the first condition is not true
+     print ((n // p) + 1) # use floor division and add 1 more
     
 
-
-# In[ ]:
 
 
 # Solution 2 - use the ceil function from the math module
