677b5ff64187f1acefff02e97abb6d761c4c1532
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-24 22:28:17 +0200

    Add files via upload

---
+++ Python Fundamentals/Data Types & Variables/05. print_part_of_the_ASCII_table.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+start_num = int(input())
+end_num = int(input())
+
+# use for loop to set the range and chr function to get the character
+for num in range (start_num, end_num + 1):
+    print (f"{chr(num)} ", end= "")
+
+
+# In[ ]:
+
+
+
+
