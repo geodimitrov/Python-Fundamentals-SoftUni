e191f9937ca0237c2859b9b284fc65fcaeecb7c6
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-24 21:45:31 +0200

    Add files via upload

---
+++ Python Fundamentals/Data Types & Variables/01. integer_operations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+# 1. Introduce the numbers
+
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
+num_4 = int(input())
+
+# 2. Calculate result
+result = (int((num_1 + num_2) / num_3))  * num_4
+
+# 3. Print result
+print (result)
+
+
+# In[ ]:
+
+
+
+
