1e812eb2de883225a04694da652e652691d79dbb
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-27 21:08:44 +0200

    Add files via upload

---
+++ Python Fundamentals/Lists/Basics/05. faro_shuffle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+cards = input().split()
+num_shuffles = int(input())
+
+# Use constants for top & bottom cards (they don't change)
+TOP_CARD = cards[0]
+BOTTOM_CARD = cards[-1]
+
+# Copy the original list of cards, remove top & bottom, split in half
+shuffled_cards = cards.copy()
+shuffled_cards.remove(TOP_CARD)
+shuffled_cards.remove(BOTTOM_CARD)
+half = len(shuffled_cards) // 2
+
+# "Shuffle" the cards
+for shuffles in range(num_shuffles):
+    
+    shuffled_cards_new = []
+    left_cards = shuffled_cards[0:half]
+    right_cards = shuffled_cards[half:len(shuffled_cards)]
+    
+    for i in range(half):
+        shuffled_cards_new.append(right_cards[i])
+        shuffled_cards_new.append(left_cards[i])
+    
+    shuffled_cards = shuffled_cards_new
+    
+# Add top & bottom back to the list
+shuffled_cards.append(BOTTOM_CARD)
+shuffled_cards.insert(0, TOP_CARD)
+
+print (shuffled_cards)
+
+
+# In[ ]:
+
+
+
+
