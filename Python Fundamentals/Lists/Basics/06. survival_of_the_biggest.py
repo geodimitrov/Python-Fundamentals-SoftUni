0866ef5e0b13fbcd99905bb8c827b83e970eb61f
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-27 21:22:42 +0200

    Add files via upload

---
+++ Python Fundamentals/Lists/Basics/06. survival_of_the_biggest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+numbers = input().split()
+n_remove = int(input())
+
+# use list comprehension to turn str to nums
+numbers_int = [int(num) for num in numbers]
+
+# use for loop to go through the list; use min func to remove the min number
+for i in range (n_remove):
+    numbers_int.remove(min(numbers_int))
+    
+print(numbers_int)
+
+
+# In[ ]:
+
+
+
+
