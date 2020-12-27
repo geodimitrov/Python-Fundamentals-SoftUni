e12fa85f6a5bd2d7dddd6a08f70d38792eb9fb56
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-27 21:33:43 +0200

    Add files via upload

---
+++ Python Fundamentals/Lists/Basics/08. seize_the_fire.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# create constants
+
+R_HIGH = range(81, 126)
+R_MEDIUM = range(51, 81)
+R_LOW = range(1, 51)
+
+# introduce input variables
+fires_data = input().split('#')
+water = int(input())
+effort = 0
+put_out_fire = []
+
+for element in fires_data:
+    type_fire, value = element.split(" = ")
+    value = int(value)
+    
+    if type_fire == "High" and value not in R_HIGH:
+        continue
+    elif type_fire == "Medium" and value not in R_MEDIUM:
+        continue
+    elif type_fire == "Low" and value not in R_LOW:
+        continue
+    
+    if water >= value:
+        water -= value
+        effort += value * 0.25
+        put_out_fire.append(value)    
+
+        
+print ("Cells:")
+for value in put_out_fire:
+    print (f" - {value}")
+
+print(f"Effort: {effort:.2f}")
+print (f"Total Fire: {sum(put_out_fire)}")
+
+
+# In[ ]:
+
+
+
+
---
+++ Python Fundamentals/Lists/Basics/09. hello_france.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+items = input().split("|")
+budget = float(input())
+total_cost = 0
+bought_items = []
+
+
+for item in items:
+    item_type, item_price = item.split("->")
+    item_price = float(item_price)
+
+    if item_type == "Clothes" and item_price > 50.00:
+        continue
+    if item_type == "Shoes" and item_price > 35.00:
+        continue
+    if item_type == "Accessories" and item_price > 20.50:
+        continue
+        
+    if budget >= item_price:
+        budget -= item_price
+        total_cost += item_price
+        bought_items.append(item_price * 1.4)
+
+profit = sum(bought_items) - total_cost
+budget += sum(bought_items)
+
+for el in bought_items:
+    print(f"{el:.2f} ", end="")
+    
+print()
+print (f"Profit: {profit:.2f}")
+
+if budget >= 150:
+    print("Hello, France!")
+else:
+    print("Time to go.")
+
+
+# In[ ]:
+
+
+
+
