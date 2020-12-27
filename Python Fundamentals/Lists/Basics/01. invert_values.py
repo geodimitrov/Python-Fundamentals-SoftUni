f307c77e50e86a7f4a35829e4222115c620dbf25
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-27 18:29:15 +0200

    Add files via upload

---
+++ Python Fundamentals/Lists/Basics/01. invert_values.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# 1. Introduce input variable
+string = input()
+
+# 2. Use the split method to split the string into list
+list_string = string.split()
+new_list = []
+
+# 3. Run a for loop to filter nums using conditions
+
+for element in list_string:
+    num = int(element)
+    if num > 0:
+        new_list.append(-num)
+    elif num < 0:
+        new_list.append(abs(num))
+    else:
+        new_list.append(num)
+        
+print(new_list)
+
+
+# In[ ]:
+
+
+
+
