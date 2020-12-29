57dca53d978bd21d43cffe4b68ef21908151d82c
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-29 12:38:20 +0200

    Add files via upload

---
+++ Python Fundamentals/Functions/05. palindrome_integers.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+#Use simple logic: check if nums in reversed order match nums in original order
+
+def palindrome_check(numbers):
+    list_nums = numbers.split(", ")
+    
+    for element in list_nums:
+        new_num = element[::-1]
+        
+        if new_num == element:
+            print ("True")
+        else:
+            print ("False")
+        
+nums = input()
+palindrome_check(nums)
+
+
+# In[ ]:
+
+
+
+
---
+++ Python Fundamentals/Functions/06. password_validator.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+def password_validator(password):
+    is_valid = True
+   
+    if not 6 <= len(password) <= 10:
+        is_valid = False
+        print("Password must be between 6 and 10 characters")
+        
+    for char in password:
+        if char.isdigit() or char.isalpha():
+            continue
+        else:
+            is_valid = False
+            print ("Password must consist only of letters and digits")
+            break
+            
+    digits = 0
+    for char in password:
+        if char.isdigit():
+            digits += 1
+    
+    if digits < 2:
+        is_valid = False
+        print ("Password must have at least 2 digits")
+        
+    return is_valid
+            
+pass_input = input()
+is_valid = password_validator(pass_input)
+
+if is_valid:
+    print ("Password is valid")
+
+
+# In[ ]:
+
+
+
+
