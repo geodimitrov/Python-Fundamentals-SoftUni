c8a9c7ae2af6ca17f9074338e3f9c4309eb0cf6c
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-29 19:38:49 +0200

    Add files via upload

---
+++ Python Fundamentals/Lists/Advanced/01. which_are_in.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+#Solution 1 - run a nested for loop
+
+list_one = input().split(", ")
+list_two = input().split(", ")
+new_list = []
+
+for el in list_one:
+    for element in list_two:
+        if el in element:
+            new_list.append(el)
+            break
+
+print(new_list)
+
+
+# In[25]:
+
+
+#Solution 2 - leave 2nd variable as str, use list comprehension
+words = input().split(", ")
+strings = input()
+
+new_list = [word for word in words if word in strings]
+
+print(new_list)
+
+
+# In[ ]:
+
+
+
+
