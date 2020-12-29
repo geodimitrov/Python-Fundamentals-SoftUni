1a686e505f45d2cbe0d85f4be45d158f0eb573b2
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-29 20:58:53 +0200

    Add files via upload

---
+++ Python Fundamentals/Lists/Advanced/02. big_numbers_lover.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+# use list comprehension to create list from input data; the elements should be str
+nums = [n for n in input().split()]
+
+# sort the list
+nums = sorted(nums, reverse=True)
+
+# use join method to join the elements of the list into a single str
+print("".join(nums))
+
+
+# In[ ]:
+
+
+
+
