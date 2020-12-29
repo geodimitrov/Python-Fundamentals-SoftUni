55a3368fe4db7f4fa0f1705c82c41192b43a8e71
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-29 22:01:50 +0200

    Add files via upload

---
+++ Python Fundamentals/Lists/Advanced/03. next_version.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+current_version = [int(el) for el in input().split(".")]
+
+for i in range(len(current_version) -1, -1, -1):
+    if current_version[i] < 9:
+        current_version[i] += 1
+        break
+    else:
+        current_version[i] = 0
+        continue
+
+new_version = ".".join([str(el) for el in current_version])
+
+print(new_version)
+
+
+# In[ ]:
+
+
+
+
---
+++ Python Fundamentals/Lists/Advanced/04. office_chairs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+n_rooms = int(input())
+
+room = 0
+free_chairs = 0
+
+for i in range(n_rooms):
+    chairs = input().split()
+    room += 1
+    
+    if len(chairs[0]) >= int(chairs[1]):
+        free_chairs += len(chairs[0]) - int(chairs[1])
+    else:
+        print(f"{int(chairs[1]) - len(chairs[0])} more chairs needed in room {room}")
+        free_chairs += len(chairs[0]) - int(chairs[1])
+    
+if free_chairs >= 0:
+    print(f"Game On, {free_chairs} free chairs left")
+
+
+# In[2]:
+
+
+x = ["xxxx", 5]
+print(len(x[0]))
+
+
+# In[ ]:
+
+
+
+
