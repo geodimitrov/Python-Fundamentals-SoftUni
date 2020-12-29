f7805f02529bd6a76e1680917c6f8bbd7c749fe5
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-29 15:52:40 +0200

    Add files via upload

---
+++ Python Fundamentals/Functions/09. factorial_division.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+# Import math module and use the factorial func
+import math
+
+def factorial_division(num_1, num_2):
+    factorial_num_1 = math.factorial(num_1)
+    factorial_num_2 = math.factorial(num_2)
+    result = factorial_num_1 / factorial_num_2
+    return (f"{result:.2f}")
+
+num_one = int(input())
+num_two = int(input())
+
+print(factorial_division(num_one, num_two))
+
+
+# In[ ]:
+
+
+
+
