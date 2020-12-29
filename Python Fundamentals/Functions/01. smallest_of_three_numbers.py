63e480acd75d84a91723f636287e5a0c918a5513
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-29 11:37:30 +0200

    Add files via upload

---
+++ Python Fundamentals/Functions/01. smallest_of_three_numbers.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# Define the function, use min function
+def smallest_of_three_nums(num_1, num_2, num_3):
+    return (min(num_1, num_2, num_3))
+
+# Introduce the numbers
+num_one = int(input())
+num_two = int(input())
+num_three = int(input())
+
+#print result
+print(smallest_of_three_nums(num_one, num_two, num_three))
+
+
+# In[ ]:
+
+
+
+
---
+++ Python Fundamentals/Functions/02. add_and_subtract.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+# Define the broader func & inside of it define the other 2 funcs
+def add_and_subtract(num_1, num_2, num_3):
+    
+    def sum_nums (num_1, num_2):
+        return num_1 + num_2
+    
+    def subtract(num_1, num_2, num_3):
+        return sum_nums(num_1, num_2) - num_3
+    
+    return subtract(num_1, num_2, num_3)
+    
+                   
+num_one = int(input())
+num_two = int(input())
+num_three = int(input())
+
+print(add_and_subtract(num_one, num_two, num_three))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
