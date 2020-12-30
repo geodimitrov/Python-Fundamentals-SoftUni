91e4655cef4def2964de4f3af4238dbf66190487
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2020-12-30 16:55:56 +0200

    Add files via upload

---
+++ Python Fundamentals/Lists/Advanced/06. group_of_tens.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+numbers = [int(n) for n in input().split(", ")]
+
+boundary = 0
+
+while numbers:
+    boundary += 10
+    current_group = []
+    for num in numbers:
+        if num <= boundary:
+            current_group.append(num)
+    for num in current_group:
+        numbers.remove(num)
+            
+    print(f"Group of {boundary}'s: {current_group}")
+
+
+# In[ ]:
+
+
+
+
