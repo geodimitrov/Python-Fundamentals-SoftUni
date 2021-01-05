3549ec226841ac197c375e51c4d511a9ecb8e01e
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-01-05 19:44:49 +0200

    Add files via upload

---
+++ Python Fundamentals/Dictionaries/05. softuni_parking.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+n = int(input())
+
+registrations = {}
+
+for i in range(n):
+    command = input()
+    command_type = command.split()[0]
+    user = command.split()[1]
+    
+    if command_type == "register": 
+        plate = command.split()[2]
+        
+        if user in registrations:
+            print(f"ERROR: already registered with plate number {plate}")
+        else:
+            registrations[user] = plate
+            print(f"{user} registered {plate} successfully")
+                
+    elif command_type == "unregister":
+        
+        if not user in registrations:
+            print(f"ERROR: user {user} not found")
+        else:
+            print(f"{user} unregistered successfully")
+            del registrations[user]
+            
+for user, plate in registrations.items():
+    print(f"{user} => {plate}")
+
+
+# In[ ]:
+
+
+
+
---
+++ Python Fundamentals/Dictionaries/06. courses.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+#The only tricky part in this problem is the sorting
+data = input()
+
+courses = {}
+
+while not data == "end":
+    course_name, student_name = data.split(" : ")
+    
+    if course_name not in courses:
+        courses[course_name] = []    
+    courses[course_name].append(student_name)
+        
+    data = input()
+
+#be careful when you sort
+sorted_courses = dict(sorted(courses.items(), key= lambda x: len(x[1]), reverse=True))
+
+for course, students in sorted_courses.items():
+    print(f"{course}: {len(students)}")
+    for name in sorted(students):
+        print(f"-- {name}")
+
+
+# In[7]:
+
+
+random = ['John Anderson', 'Andrew Robinson', 'Clark Lewis']
+print(sorted(random))
+
+
+# In[ ]:
+
+
+
+
