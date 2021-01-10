f51f68ad668bfc8643d78eedec24739982279f9e
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-01-10 14:44:48 +0200

    Update 02. exchange_integers.py

--- Python Fundamentals/Data Types & Variables/More Exercises/02. exchange_integers.py
+++ Python Fundamentals/Data Types & Variables/More Exercises/02. exchange_integers.py
@@ -1,18 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
 
 num_1 = int(input())
 num_2 = int(input())
 
 print (f"Before:\na = {num_1}\nb = {num_2}")
 print (f"After:\na = {num_2}\nb = {num_1}")
-
-
-# In[ ]:
-
-
-
-
