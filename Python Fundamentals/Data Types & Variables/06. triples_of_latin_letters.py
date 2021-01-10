18eef12d662f80afd90485c523d918a91e1ef916
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-01-10 14:33:06 +0200

    Update 06. triples_of_latin_letters.py

--- Python Fundamentals/Data Types & Variables/06. triples_of_latin_letters.py
+++ Python Fundamentals/Data Types & Variables/06. triples_of_latin_letters.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
 
 n = int(input())
 
@@ -10,10 +5,4 @@ for i in range (n):
     for k in range (n):
         for y in range (n):
             print (f"{chr(97 + i)}{chr(97 + k)}{chr(97 + y)}")
-
-
-# In[ ]:
-
-
-
 
