9eea2d14034da7892fc8b7227aaa31f9fe0b9bea
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-01-10 13:26:24 +0200

    Add files via upload

---
+++ Python Fundamentals/Text Processing/03. extract_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+file_path = input().split("\\")
+                          
+file_name, file_ext = file_path[-1].split(".")
+
+print(f"File name: {file_name}")
+print(f"File extension: {file_ext}")
+
+
+# In[ ]:
+
+
+
+
---
+++ Python Fundamentals/Text Processing/04. caesar_cipher.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Solution 1
+text = input()
+
+new_text = ""
+
+for char in text:
+    new_char = chr(ord(char) + 3)
+    new_text += new_char
+    
+print(new_text)
+
+
+# In[5]:
+
+
+#Solution 2
+
+text = input()
+
+for char in text:
+    new_char = chr(ord(char) + 3)
+    print(new_char, end="")
+
+
+# In[ ]:
+
+
+
+
