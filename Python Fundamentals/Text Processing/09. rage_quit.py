5f918da9fbdfe2b5a6731b76369f72daa008ccd3
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-01-10 13:42:02 +0200

    Update 09. rage_quit.py

--- Python Fundamentals/Text Processing/09. rage_quit.py
+++ Python Fundamentals/Text Processing/09. rage_quit.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[26]:
-
 
 data = input().upper()
 
@@ -26,10 +21,4 @@ for i in range(len(data)):
 
 print(f"Unique symbols used: {len(set(final_result))}")
 print(f"{final_result}")
-
-
-# In[ ]:
-
-
-
 
