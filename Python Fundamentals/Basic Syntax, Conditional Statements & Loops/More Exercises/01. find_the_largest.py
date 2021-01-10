44660f07a7680de290ce613c615d17d91f993b41
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-01-10 14:20:56 +0200

    Add files via upload

---
+++ Python Fundamentals/Basic Syntax, Conditional Statements & Loops/More Exercises/01. find_the_largest.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+#Solution 1
+# since it's not explicitly specified in description, we will not nitroduce a num but str
+number = input()
+
+# turn the str into a list (use list comprehension)
+number = [digit for digit in number]
+
+# sort the list in reverse
+sorted_number = sorted(number, reverse=True)
+
+#turn the sorted list into a number
+largest_number = int("".join(sorted_number))
+
+print(largest_number)
+
+
+# In[12]:
+
+
+# Solution 2
+# you can directly turn the var value into a list
+
+number = [digit for digit in input()]
+
+sorted_number = sorted(number, reverse=True)
+
+print("".join(sorted_number))
+
+
+# In[ ]:
+
+
+
+
