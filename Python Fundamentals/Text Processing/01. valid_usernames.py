5ce1d97a9f8d6add27900ba5255e553592c20606
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-01-10 13:20:07 +0200

    Add files via upload

---
+++ Python Fundamentals/Text Processing/01. valid_usernames.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+usernames = input().split(", ")
+
+
+for username in usernames:
+    valid = False
+    
+    if 3 < len(username) < 16:
+        for char in username:
+            if char.isalpha() or char.isdigit() or char == "-" or char == "_":
+                valid = True
+            else:
+                valid = False
+                break
+            
+    if valid:
+        print(username)
+
+
+# In[ ]:
+
+
+
+
---
+++ Python Fundamentals/Text Processing/02. character_multiplier.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+# Solution 1
+
+text_1, text_2 = input().split()
+
+sum_chars = 0
+
+if len(text_1) > len(text_2):
+    for i in range(len(text_2)):
+        sum_chars += ord(text_1[i]) * ord(text_2[i])
+    for i in range(len(text_2), len(text_1)):
+        sum_chars += ord(text_1[i])
+else:
+    for i in range(len(text_1)):
+        sum_chars += ord(text_1[i]) * ord(text_2[i])
+    for i in range(len(text_1), len(text_2)):
+        sum_chars += ord(text_2[i])
+
+print(sum_chars)
+
+
+# In[14]:
+
+
+#Solution 2 (with a function)
+text_1, text_2 = input().split()
+
+
+def char_multiplier(text_one, text_two):
+    sum_chars = 0
+    for i in range(len(text_two)):
+        sum_chars += ord(text_one[i]) * ord(text_two[i])
+    for i in range(len(text_two), len(text_one)):
+        sum_chars += ord(text_one[i])
+    return sum_chars
+
+if len(text_1) > len(text_2):
+    print(char_multiplier(text_1, text_2))
+else:
+    print(char_multiplier(text_2, text_1))
+
+
+# In[ ]:
+
+
+
+
