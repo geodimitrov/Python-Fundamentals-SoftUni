cccc6c7b999219319a9324e5e1022aa72e69970c
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-01-10 13:53:27 +0200

    Update 08. mutate_strings.py

--- Python Fundamentals/Basic Syntax, Conditional Statements & Loops/08. mutate_strings.py
+++ Python Fundamentals/Basic Syntax, Conditional Statements & Loops/08. mutate_strings.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
 
 
-# 1. Introduce variables (2 strings)
+# Solution 1
 
 text_one = input()
 text_two = input()
@@ -15,10 +11,7 @@ for i in range(len(text_one)):
         print(text_one)
 
 
-# In[ ]:
-
-
-#Solution 2
+# Solution 2
 
 string_one = input()
 string_two = input()
@@ -28,10 +21,4 @@ for i in range(len(string_one)):
     if not string_one[i] == string_two[i]:
         new_string[i] = string_two[i]
         print("".join(new_string))
-
-
-# In[ ]:
-
-
-
 
