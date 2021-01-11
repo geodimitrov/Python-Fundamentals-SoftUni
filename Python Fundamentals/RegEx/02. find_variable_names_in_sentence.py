00c10e1adff00f882bfe44a847108e26466501b4
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-01-11 20:29:29 +0200

    Update 02. find_variable_names_in_sentence.py

--- Python Fundamentals/RegEx/02. find_variable_names_in_sentence.py
+++ Python Fundamentals/RegEx/02. find_variable_names_in_sentence.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import re
 
@@ -14,14 +9,3 @@ matches = [match.group() for match in re.finditer(ex, data)]
 matches = [el.replace("_", "")for el in matches]
 
 print(*matches, sep=",")
-
-
-# In[ ]:
-
-
-import re
-
-data = input()
-
-ex = r""
-
