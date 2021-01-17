8a350ee0cc4e9b70c11b28f8de805a00ad9b74dc
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-01-17 12:04:02 +0200

    Update 02. sets_of_elements.py

--- Python Advanced/Tuples & Sets/02. sets_of_elements.py
+++ Python Advanced/Tuples & Sets/02. sets_of_elements.py
@@ -1,14 +1,14 @@
 #create a func to find num of elements in each set
-
 def find_numbers():
     nums = tuple(map(int, input().split()))
     return nums
-
+#create a func to fill in the sets
 def fill_set(count):
     s = set([])
     for _ in range(count):
         s.add(input())
     return s
+#create func to find the intersection of the sets, use intersection method
 def find_intersection(s1, s2):
     result = s1.intersection(s2)
     for el in result:
