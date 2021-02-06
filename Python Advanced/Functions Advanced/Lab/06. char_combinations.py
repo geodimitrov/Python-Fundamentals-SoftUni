ba01332f98b563c0a17aeea05a539e54a2980e20
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-02-06 12:49:10 +0200

    Update 06. char_combinations.py

--- Python Advanced/Functions Advanced/Lab/06. char_combinations.py
+++ Python Advanced/Functions Advanced/Lab/06. char_combinations.py
@@ -1,3 +1,5 @@
+# Solution 1
+# use func permutations from itertools module
 from itertools import permutations
 
 def text_permutation(string):
@@ -5,3 +7,18 @@ def text_permutation(string):
 
 text = input()
 [print("".join(el)) for el in text_permutation(text)]
+
+# Solution 2
+# use recursion
+def char_combinations(text, index=0):
+    if index == len(text):
+        print("".join(text))
+        return
+
+    for i in range(index, len(text)):
+        text[index], text[i] = text[i], text[index]
+        char_combinations(text, index + 1)
+        text[index], text[i] = text[i], text[index]
+
+text = list(input())
+char_combinations(text)
