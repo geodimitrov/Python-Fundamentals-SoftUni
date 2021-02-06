2b41dc2d96a39b72e9deca74f9073189ff832c8c
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-02-06 14:54:17 +0200

    Update 04. negative_vs_positive.py

--- Python Advanced/Functions Advanced/Exercises/04. negative_vs_positive.py
+++ Python Advanced/Functions Advanced/Exercises/04. negative_vs_positive.py
@@ -1,24 +1,16 @@
 def split_negative_from_positive(nums):
-    negative_nums = []
-    positive_nums = []
-    for num in nums:
-        if num < 0:
-            negative_nums.append(num)
-        elif num > 0:
-            positive_nums.append(num)
+    negative_nums = sum(filter(lambda x: x < 0, nums))
+    positive_nums = sum(filter(lambda x: x > 0, nums))
     return negative_nums, positive_nums
 
-def print_result(sum_negativ, sum_positiv):
-    print(sum_negativ)
-    print(sum_positiv)
-    if abs(sum_negativ) > sum_positiv:
+def print_result(sum_negative, sum_positive):
+    print(sum_negative)
+    print(sum_positive)
+    if abs(sum_negative) > sum_positive:
         print("The negatives are stronger than the positives")
     else:
         print("The positives are stronger than the negatives")
 
-nums = [int(el) for el in input().split()]
-negative_nums, positive_nums = split_negative_from_positive(nums)
-
-sum_positives = sum(positive_nums)
-sum_negatives = sum(negative_nums)
-print_result(sum_negatives, sum_positives)
+numbers = [int(el) for el in input().split()]
+sum_negative, sum_positive = split_negative_from_positive(numbers)
+print_result(sum_negative, sum_positive)
