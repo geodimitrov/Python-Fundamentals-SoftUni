d9ecf165d86c1711abb203767291826967b8e6a8
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-02-06 11:38:05 +0200

    Update 07. chairs.py

--- Python Advanced/Functions Advanced/Lab/07. chairs.py
+++ Python Advanced/Functions Advanced/Lab/07. chairs.py
@@ -1,3 +1,6 @@
+# Solution 1
+# use the func combinations from itertools module
+
 from itertools import combinations
 
 def chair_combinations(names, n_chairs):
@@ -6,3 +9,23 @@ def chair_combinations(names, n_chairs):
 names = input().split(", ")
 n = int(input()) #num of chairs
 [print(*el, sep=", ") for el in chair_combinations(names, n)]
+
+
+# Solution 2
+# Use recursion
+
+def calc_combinations(names, n, combs=[]):
+    if len(combs) == n:
+        print(", ".join(combs))
+        return
+
+    for i in range(len(names)):
+        name = names[i]
+        combs.append(name)
+        calc_combinations(names[i+1:], n, combs)
+        combs.pop()
+
+names = input().split(", ")
+n = int(input())
+
+calc_combinations(names, n)
