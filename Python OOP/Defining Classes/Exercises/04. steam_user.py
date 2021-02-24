6b8408c9ec95398c6a9c99f289b739a5560efd20
Georgi Dimitrov <74505522+geodimitrov@users.noreply.github.com>
2021-02-24 14:15:15 +0200

    Update 04. steam_user.py

--- Python OOP/Defining Classes/Exercises/04. steam_user.py
+++ Python OOP/Defining Classes/Exercises/04. steam_user.py
@@ -6,18 +6,19 @@ class SteamUser:
         self.played_hours = 0
 
     def play(self, game, hours):
-        if game in self.games:
-            self.played_hours += hours
-            return f"{self.username} is playing {game}"
-        else:
+        if game not in self.games:
             return f"{game} is not in library"
+        
+        self.played_hours += hours
+        return f"{self.username} is playing {game}"
+            
 
     def buy_game(self, game):
         if game not in self.games:
             self.games.append(game)
             return f"{self.username} bought {game}"
-        else:
-            return f"{game} is already in your library"
+   
+        return f"{game} is already in your library"
 
     def stats(self):
         return f"{self.username} has {len(self.games)} games. Total play time: {self.played_hours}"
