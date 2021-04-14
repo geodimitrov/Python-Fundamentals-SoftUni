79b0db677f57d2b42ead2a890f12776d1eb934f9
geodimitrov <geo.dimitrov@yahoo.com>
2021-04-14 15:08:14 +0300

    making changes

--- Lists/Basics/More Exercises/05. hungry_hippos.py
+++ Lists/Basics/More Exercises/05. hungry_hippos.py
@@ -1,31 +1,12 @@
-# create deltas to check vertically as well as horizontally
-deltas = [
-    (0, -1),
-    (-1, -1),
-    (-1, 0),
-    (-1, +1),
-    (0, +1),
-    (+1, +1),
-    (+1, 0),
-    (+1, -1)
+
+matrix = [
+    [1, 1, 0, 0, 0],
+    [1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1],
+    [0, 0, 0, 1, 1]
 ]
 
-#use a func to create board
-def create_board(n_rows):
-    result = []
-    for row in range(n_rows):
-        result.append([int(el) for el in input().split()])
-    return result
-
-
-
-n = 5  # number of rows
-board = create_board(n)
-print(board)
-
-for row in range(n):
-    for c in range(n):
-        if board[row][c] == "1":
-            check_deltas()
+size = 5
 
 
