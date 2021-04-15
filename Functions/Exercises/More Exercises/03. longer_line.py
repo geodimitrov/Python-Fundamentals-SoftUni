9d6d6512c5e9c3ed5bd46762305acd5b89f11ba7
geodimitrov <geo.dimitrov@yahoo.com>
2021-04-15 17:35:46 +0300

    Adding solutions

--- Functions/Exercises/More Exercises/03. longer_line.py
+++ Functions/Exercises/More Exercises/03. longer_line.py
@@ -15,17 +15,35 @@ def split_points_pairs(pairs):
 
     return pair_one, pair_two
 
-# def find_closest_point_to_center(first_point, second_point):
-#     dist_first_point = math.sqrt(((0 - first_point[0]) ** 2) + ((0 - first_point[1]) ** 2))
-#     dist_second_point = math.sqrt(((0 - second_point[0]) ** 2) + ((0 - second_point[1]) ** 2))
-#
-#     if dist_first_point <= dist_second_point:
-#         return first_point
-#
-#     return second_point
+def format_point(first_point, second_point, dist_first_point, dist_second_point):
+    if dist_first_point > dist_second_point:
+        first_point, second_point = second_point, first_point
 
+    result = f"{tuple(math.floor(el) for el in first_point)}{tuple(math.floor(el) for el in second_point)}"
+    return result
+
+def get_line_from_points(points):
+    first_point, second_point = points
+    dist_first_point = math.sqrt(((0 - first_point[0]) ** 2) + ((0 - first_point[1]) ** 2))
+    dist_second_point = math.sqrt(((0 - second_point[0]) ** 2) + ((0 - second_point[1]) ** 2))
+    points = format_point(first_point, second_point, dist_first_point, dist_second_point)
+    line = dist_first_point + dist_second_point
+
+    return line, points
+
+def compare_lines_from_points(pair_one, pair_two):
+    line_one, formatted_pair_one = get_line_from_points(pair_one)
+    line_two, formatted_pair_two = get_line_from_points(pair_two)
+
+    if line_one > line_two:
+        return formatted_pair_one
+
+    return formatted_pair_two
+
+def print_result(result):
+    print(result)
 
 points_pairs = read_input()
 pair_one, pair_two = split_points_pairs(points_pairs)
-
-
+result = compare_lines_from_points(pair_one, pair_two)
+print_result(result)
